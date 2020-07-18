3a59958b55aa062fa654b5d1c3a0c65fcf34002e
ydauracontreras <ydauracontreras@gmail.com>
2020-07-18 13:22:33 -0300

    Creacion de testUnitario para comprobar el funcionamiento del codigo

--- src/test/java/ar/com/ada/api/cursos/DemoApplicationTests.java
+++ src/test/java/ar/com/ada/api/cursos/DemoApplicationTests.java
@@ -1,13 +1,66 @@
 package ar.com.ada.api.cursos;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import ar.com.ada.api.cursos.entities.Categoria;
+import ar.com.ada.api.cursos.services.CategoriaService;
 
 @SpringBootTest
 class DemoApplicationTests {
+	@Autowired
+	CategoriaService CategoriaService;
 
 	@Test
-	void contextLoads() {
+	void crearCategoriaSinCursoTest() {
+		tituloTestFin("Categoria sin curso");
+
+		Categoria categoria = CategoriaService.crearCategoria("Matematica", "Vemos Algebra");
+		// si id era int hubiera sido assertTrue(categoria.getCategoriaId() > 0)
+		// como el id es Integer se utiliza el compareTo que devuelve:
+		// -1 si a < parametro
+		// 0 si a == 1
+		// 1 si a > parametro
+		assertTrue(categoria.getCategoriaId().compareTo(0) == 1);
+		Integer nuevaCategoriaId = categoria.getCategoriaId();
+
+		// Busca la categoria que tiene el ID en la base de datos
+		// particularmente el findById del repo: devuelve un Optional
+		// Optional va a ver que checkear si el valor existe o no.
+		/*
+		 * Optional<Categoria> catDesdeDBResultado =
+		 * repoCategoria.findById(nuevaCategoriaId);
+		 * assertTrue(catDesdeDBResultado.isPresent());
+		 */
+
+		Categoria categoriaDesdeDB = CategoriaService.buscarPorId(nuevaCategoriaId);
+		assertTrue(categoriaDesdeDB != null);
+
+		assertEquals("Matematicas", categoriaDesdeDB.getNombre());
+		assertEquals("vemos algebra", categoriaDesdeDB.getDescripcion());
+
+		tituloTestFin("Categoria sin Curso");
+	}
+
+	void tituloTestInicio(String titulo) {
+		System.out.println("***************************");
+		System.out.println("***************************");
+		System.out.println("INICIO TEST:" + titulo);
+		System.out.println("***************************");
+		System.out.println("***************************");
+
+	}
+
+	void tituloTestFin(String titulo) {
+		System.out.println("***************************");
+		System.out.println("***************************");
+		System.out.println("FIN TEST:" + titulo);
+		System.out.println("***************************");
+		System.out.println("***************************");
+
 	}
 
 }
